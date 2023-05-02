135064de15a65dc1c8a95048d65353d34debb025
wrapperup <wrapperup4@gmail.com>
2023-05-02 00:59:08 -0400

    init commit
    update readme
    
    update readme
    
    init commit
    
    Update readme.md
    
    Update readme.md

---
+++ lib/types.d.ts
@@ -0,0 +1,26 @@
+import type { FastifyInstance, FastifyServerOptions } from 'fastify';
+import type { AstroIntegration } from 'astro';
+
+export type ServerArgs = {
+  clientRelative: string;
+  assetsPrefix: string;
+  port: number | undefined;
+};
+
+export type IntegrationOptions = {
+  /**
+   * The entrypoint to where your fastify routes are defined
+   */
+  entry: string | URL;
+  /**
+   * The port to use in __production__. In development mode fastify runs
+   * on the Vite server.
+   * 
+   * By default @matthewp/astro-fastify uses process.env.PORT which most hosts will
+   * define, and you don't need to set this. If you do set this option it will override
+   * any host variables.
+   */
+  port?: number;
+};
+
+export default function(opts: IntegrationOptions): AstroIntegration;
---
+++ readme.md
@@ -0,0 +1,14 @@
+# astro-koa
+HIGHLY EXPERIMENTAL! Use at your own risk.
+
+Based on https://github.com/matthewp/astro-fastify/
+
+## Demo
+https://user-images.githubusercontent.com/7478134/235583490-b58ee25e-bc81-4db1-8840-2b75d1cf47ca.mp4
+
+## Features
+- Embed your entire Koa app
+- Hot-reloads your Koa app
+- Supports koa middleware
+  - Unofficial `Astro.locals` support for accessing your context (requires fork).
+- Supports `koa-router`
